a1afc90dd2f68bf867481eddb21a0c3769c6ca73
Angelocho <59256310+Angelocho@users.noreply.github.com>
2021-10-25 10:44:20 +0200

    Indice

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/estilo.css">
+        <script src="js/prog.js"></script>
+    </head>
+    <body>
+        <h1>Esto es un título</h1>
+        <p>Esto es un párrafo</p>
+
+        <pre>
+            Ejecuta php -S localhost:9000
+            visital con tu navegador
+        </pre>
+        <?php
+            echo "Esto lo escribe PHP"
+        ?>
+    </body>
+</html>
